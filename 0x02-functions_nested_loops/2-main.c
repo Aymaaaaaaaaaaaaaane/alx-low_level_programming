3f985ad61d98ce8ea20456b0a27abcbd407b9130
aymane raddi <aymaneraddi1999@gmail.com>
2023-06-21 03:49:26 -0700

    fixing compiler

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,16 +1,12 @@
 #include "main.h"
 
 /**
-*main - Entry point of the program
-*
-*Description: This function calls the print_alphabet_x10 function and
-*returns 0 to indicate successful execution.
-*
-*Return: Always 0
-*/
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-print_alphabet_x10();
-return (0);
+	print_alphabet_x10();
+	return (0);
 }
-
