14f6ab9af3211d9507c35c0bd9186f219a0a964e
aymane raddi <aymaneraddi1999@gmail.com>
2023-06-21 04:50:24 -0700

    fix error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - checks for alphabetic character
  * @c: the character to be checked
  *
- * Description: This function checks if the given character 'c' is an alphabetic letter.
+ * Description:  checks if the given character 'c' is an alphabetic letter.
  * It returns 1 if 'c' is a letter, and 0 otherwise.
  *
  * Return: 1 if 'c' is a letter, 0 otherwise
