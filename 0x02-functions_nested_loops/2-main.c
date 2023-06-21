16fa9cfb0c5291a00e3d3d0a17ed60ea89fe06ff
aymane raddi <aymaneraddi1999@gmail.com>
2023-06-21 03:47:14 -0700

    fixing betty style

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
+/**
+*main - Entry point of the program
+*
+*Description: This function calls the print_alphabet_x10 function and
+*returns 0 to indicate successful execution.
+*
+*Return: Always 0
+*/
+int main(void)
+{
+print_alphabet_x10();
+return (0);
+}
 
-	/**
-	 * main - check the code.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
-	{
-	    print_alphabet_x10();
-	    return (0);
-	}
